654bf169d54d75dbcac5287ccbb01f5f82ce4b02
shriram0205 <shriram0205@gmail.com>
2017-01-11 18:54:49 -0800

    webpack cool app

---
+++ src/js/ReactComponent.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+ 
+const ReactComponent = () => (
+  <RaisedButton label="Primary" />
+);
+export default ReactComponent;
